67a6e3f40316aafcab1df1b1ab0fc4d573751d41
陈益飞 <3056000855@qq.com>
2026-01-04 11:53:08 +0800

    添加相机控制父类CameraControl

---
+++ application/camera/cameraControl.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "../../glframework/core.h"
+#include "camera.h"
+#include <map>
+
+class CameraControl
+{
+public:
+	CameraControl();
+	~CameraControl();
+
+	virtual void onMouse(int button, int action ,double xpos, double ypos);
+	virtual void onCursor(double xpos, double ypos);
+	virtual void onKey(int key, int action, int mods);
+
+	virtual void update();
+
+	void setCamera(Camera* camera) { mCamera = camera; }
+	void setSensitivity(float sensitivity) { mSensitivity = sensitivity; }
+
+protected:
+	bool mLeftMouseDown=false;
+	bool mRightMouseDown = false;
+	bool mMiddleMouseDown = false;
+
+	float mCurrentX = 0.0f;
+	float mCurrentY = 0.0f;
+
+	float mSensitivity = 0.2f;
+
+	std::map<int, bool> mKeyMap;
+
+	Camera* mCamera = nullptr;
+};
